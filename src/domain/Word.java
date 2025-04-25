2573c7297d549731e511b0f92086980f1ab02829
NM715 <Nawal.Mohamuud@edu.sait.ca>
2025-04-25 17:58:12 -0600

    addedjavadoc

--- src/domain/Word.java
+++ src/domain/Word.java
@@ -6,29 +6,64 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Represents a word and tracks its occurrences across different files.
+ */
 public class Word implements Comparable<Word>, Serializable {
     private static final long serialVersionUID = 1L;
     
     private String word;
     private Map<String, List<Integer>> fileOccurrences;
     
+    /**
+     * Creates a new Word with the specified text.
+     * @param word
+     * Precondition: word is not null
+     * Postcondition: A new Word object is created with the specified text and empty occurrences
+     */
     public Word(String word) {
         this.word = word;
         this.fileOccurrences = new HashMap<>();
     }
     
+    /**
+     * Record the occurrence of the word at specified filename and line occurrence.
+     * @param filename
+     * @param lineNumber
+     * Precondition: filename is not null
+     * Postcondition: The occurrence is recorded in the fileOccurrences map
+     */
     public void addOccurrence(String filename, int lineNumber) {
         fileOccurrences.computeIfAbsent(filename, k -> new ArrayList<>()).add(lineNumber);
     }
     
+    /**
+     * Gets the text of this word.
+     * @return The word's text
+     * Precondition: The Word object is initialized
+     * Postcondition: The text of the word is returned
+     */
     public String getWord() {
         return word;
     }
     
+    /**
+     * Gets the map of file occurrences for the word.
+     * @return A map with filenames as keys and lists of line numbers as values
+     * Precondition: The Word object is initialized
+     * Postcondition: The map of file occurrences is returned
+     */
     public Map<String, List<Integer>> getFileOccurrences() {
         return fileOccurrences;
     }
     
+  
+    /**
+     * Calculates the words total number of occurrences.
+     * @return total frequency count of the word
+     * Precondition: The word object is initialized
+     * Postcondition: The total number of occurrences is returned
+     */
     public int getTotalFrequency() {
         int total = 0;
         for (List<Integer> lines : fileOccurrences.values()) {
@@ -37,15 +72,28 @@ public class Word implements Comparable<Word>, Serializable {
         return total;
     }
     
+  
+    /**
+     * Gets a list of all files where this word occurs.
+     * @return A list of filenames
+     * Precondition: The word object is initialized
+     * Postcondition: A list containing the names of all files where this word occurs is returned
+     */
     public List<String> getFiles() {
         return new ArrayList<>(fileOccurrences.keySet());
     }
     
+    /**
+     * Compares this word to another word alphabetically.
+     * @return the compared result
+     * Precondition: other is not null
+     * Postcondition: The result is returned
+     */
     @Override
     public int compareTo(Word other) {
         return this.word.compareTo(other.word);
     }
-
+    
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
--- src/serialization/SerializeBSTree.java
+++ src/serialization/SerializeBSTree.java
@@ -3,8 +3,22 @@ package serialization;
 import java.io.*;
 import implementations.BSTree;
 
+
+/**
+ * Provides serialization and deserialization functionality for Binary Search Tree objects.
+ * Allows saving and loading binary search trees to and from files.
+ */
+ 
 public class SerializeBSTree {
 
+	/**
+	 * Serializes a Binary Search Tree to a file.
+	 * @param tree
+	 * @param filename
+	 * @throws IOException If an I/O errors occurs
+     * Precondition: Tree object exists and filename is valid
+     * Postcondition: The tree is serialized and saved to the specified file
+	 */
     public static void serialize(BSTree<?> tree, String filename) {
         try (ObjectOutputStream oos = new ObjectOutputStream(
                 new FileOutputStream(filename))) {
@@ -14,6 +28,15 @@ public class SerializeBSTree {
         }
     }
 
+    /**
+     * Deserializes a Binary Search Tree to a file.
+     * @param filename
+     * @return The deserialized BSTree or null if error occurs
+     * @throws IOException If an I/O error occurs
+     * @throws ClassNotFoundException If the class of the serialized object cannot be found
+     * Precondition: The file exists and contains a serialized BSTree object
+     * Postcondition: The BSTree object is created from the file, or null is returned if an error occurs
+     */
     @SuppressWarnings("unchecked")
     public static BSTree<?> deserialize(String filename) {
         try (ObjectInputStream ois = new ObjectInputStream(
